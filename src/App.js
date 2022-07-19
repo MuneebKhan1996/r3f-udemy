6b7ea08e219c50e99921f4fcca8d0fb3980685d2
MuneebKhan1996 <rmkhan1996@hotmail.com>
2022-07-19 11:43:41 +0500

    react 3

--- src/App.js
+++ src/App.js
@@ -1,46 +1,76 @@
-import logo from './logo.svg';
+//@ts-check
 import './App.css';
+import { Canvas, useFrame, extend, useThree } from '@react-three/fiber';
+import { useRef } from 'react';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import * as THREE from 'three';
+extend({ OrbitControls });
+
+const Orbit = () => {
+	const { camera, gl } = useThree();
+	return <orbitControls args={[camera, gl.domElement]} />;
+};
+
+const Box = (props) => {
+	const ref = useRef();
+
+	useFrame((state) => {
+		ref.current.rotation.y += 0.01;
+	});
+
+	return (
+		<mesh ref={ref} {...props} castShadow receiveShadow>
+			<boxGeometry />
+			<meshStandardMaterial color={'red'} opacity={0.5} transparent />
+		</mesh>
+	);
+};
+
+const Floor = (props) => {
+	return (
+		<mesh {...props} receiveShadow>
+			<boxBufferGeometry args={[10, 0.1, 10]} />
+			<meshPhysicalMaterial />
+		</mesh>
+	);
+};
+
+const Bulb = (props) => {
+	return (
+		<mesh {...props}>
+			<pointLight castShadow />
+			<sphereBufferGeometry args={[0.5, 20, 20]} />
+			<meshPhongMaterial emissive={'yellow'} />
+		</mesh>
+	);
+};
 
 function App() {
-	const scene = new THREE.Scene();
-	const camera = new THREE.PerspectiveCamera(
-		75,
-		window.innerWidth / window.innerHeight,
-		0.1,
-		1000
+	const points = [];
+	points.push(new THREE.Vector3(-1, 0, 0));
+	points.push(new THREE.Vector3(0, 1, 0));
+	points.push(new THREE.Vector3(1, 0, 0));
+	points.push(new THREE.Vector3(0, 5, 0));
+
+	const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+	return (
+		<div style={{ height: '100vh', width: '100vw' }}>
+			<Canvas
+				style={{ background: 'black' }}
+				camera={{ position: [1, 3, 1] }}
+				shadows
+			>
+				<fog attach='fog' args={['white', 1, 10]} />
+				<ambientLight intensity={0.2} />
+				<Box position={[0, 1, 0]} />
+				<Bulb position={[0, 3, 0]} />
+				<Orbit />
+				<axesHelper args={[5]} />
+				<Floor position={[0, -0.5, 0]} />
+			</Canvas>
+		</div>
 	);
-	const renderer = new THREE.WebGLRenderer();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-
-	document.body.innerHTML = '';
-	document.body.appendChild(renderer.domElement);
-
-	const geometry = new THREE.BoxGeometry();
-	const material = new THREE.MeshBasicMaterial({
-		color: 'blue',
-	});
-
-	const cube = new THREE.Mesh(geometry, material);
-	scene.add(cube);
-	camera.position.z = 5;
-
-	function animate() {
-		requestAnimationFrame(animate);
-		cube.rotation.x += 0.01;
-		cube.rotation.y += 0.01;
-		renderer.render(scene, camera);
-	}
-
-	animate();
-
-	window.addEventListener('resize', () => {
-		renderer.setSize(window.innerWidth, window.innerHeight);
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-	});
-
-	return null;
 }
 
 export default App;
